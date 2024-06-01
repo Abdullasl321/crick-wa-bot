f8298f36134e4aaf40e9b11623066764ce62e04e
Abdullasl321 <108709846+Abdullasl321@users.noreply.github.com>
2024-06-01 19:59:45 +0530

    PICXGRAPHY | UNIQUE WORLD

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const countryEmoji = require('country-emoji');
 const cron = require('node-cron');
 const moment = require('moment-timezone');
 
+
 const API_KEY = 'bddc3363-551c-4a1b-b5f5-7d809e727e19';
 const API_URL = `https://api.cricapi.com/v1/cricScore?apikey=${API_KEY}`;
 
@@ -28,6 +29,8 @@ async function connectToWhatsApp() {
         version: version
     });
 
+    sock.ev.on('creds.update', saveCreds);
+
     sock.ev.on('connection.update', async (update) => {
         const { connection, lastDisconnect, qr } = update;
 
@@ -41,12 +44,10 @@ async function connectToWhatsApp() {
         }
     });
 
-    sock.ev.on('creds.update', saveCreds);
-
-    sock.ev.on('messages.upsert', ({ messages }) => {
+    sock.ev.on('messages.upsert', async ({ messages }) => {
         const remoteJid = messages[0].key.remoteJid;
 
-        messages.forEach(msg => {
+        messages.forEach(async (msg) => {
             if (msg.message && msg.message.conversation) {
                 const messageBody = msg.message.conversation;
 
@@ -67,52 +68,49 @@ async function connectToWhatsApp() {
 
                     const matchId = command;
                     const startTime = args[2];
+                    const updateDuration = args[3] || '5'; // Default update duration is 5 minutes
                     if (!matchId || !startTime) {
-                        sock.sendMessage(remoteJid, { text: `🛑 *MATCH ID OR START TIME NOT PROVIDED* 🛑\n\n> .livescore {match_id} {start_time}` });
+                        sock.sendMessage(remoteJid, { text: `🛑 *MATCH ID OR START TIME NOT PROVIDED* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
                         return;
                     }
 
                     const startTimeSL = moment.tz(startTime, 'HH:mm', 'Asia/Colombo');
 
-                    const task = cron.schedule('*/5 * * * *', () => {
+                    const task = cron.schedule(`*/${updateDuration} * * * *`, async () => {
                         const nowSL = moment.tz('Asia/Colombo');
                         if (nowSL.isBefore(startTimeSL)) return;
 
-                        axios.get(`${API_URL}&id=${matchId}`)
-                            .then(response => {
-                                const matches = response.data.data;
+                        try {
+                            const response = await axios.get(`${API_URL}&id=${matchId}`);
+                            const matches = response.data.data;
 
-                                if (!matches || matches.length === 0) {
-                                    console.error('No matches data found');
-                                    sock.sendMessage(remoteJid, { text: `🛑 *NO MATCH DATA FOUND* 🛑\n\n> .livescore {match_id} {start_time}` });
-                                    return;
-                                }
+                            if (!matches || matches.length === 0) {
+                                console.error('No matches data found');
+                                sock.sendMessage(remoteJid, { text: `🛑 *NO MATCH DATA FOUND* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
+                                return;
+                            }
 
-                                const match = matches.find(m => m.id === matchId);
+                            const match = matches.find(m => m.id === matchId);
 
-                                if (!match) {
-                                    console.error('No match data found');
-                                    sock.sendMessage(remoteJid, { text: `🛑 *NO MATCH DATA FOUND* 🛑\n\n> .livescore {match_id} {start_time}` });
-                                    return;
-                                }
+                            if (!match) {
+                                console.error('No match data found');
+                                sock.sendMessage(remoteJid, { text: `🛑 *NO MATCH DATA FOUND* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
+                                return;
+                            }
 
-                                const team1 = match.t1;
-                                const team2 = match.t2;
-                                const status = match.status;
-                                const series = match.series;
-                                const team1Score = match.t1s || 'N/A';
-                                const team2Score = match.t2s || 'N/A';
+                            const team1 = match.t1;
+                            const team2 = match.t2;
+                            const status = match.status;
+                            const series = match.series;
+                            const team1Score = match.t1s || 'N/A';
+                            const team2Score = match.t2s || 'N/A';
 
-                                const team1Name = team1.match(/(.*?) \[/)[1];
-                                const team2Name = team2.match(/(.*?) \[/)[1];
+                            const team1Name = team1.match(/(.*?) \[/)[1];
+                            const team2Name = team2.match(/(.*?) \[/)[1];
 
-                                const team1Emoji = countryEmoji.flag(team1Name) || '';
-                                const team2Emoji = countryEmoji.flag(team2Name) || '';
+                            const liveUpdate = `            🏏 *𝘓𝘐𝘝𝘌 𝘜𝘗𝘋𝘈𝘛𝘌𝘚* 🏏
 
-                                const liveUpdate = `
-            🏏 *𝘓𝘐𝘝𝘌 𝘜𝘗𝘋𝘈𝘛𝘌𝘚* 🏏
-
-*${team1Name}* ${team1Emoji} 𝘝𝘚 *${team2Name}* ${team2Emoji}
+*${team1Name}* vs *${team2Name}*
 (${series})
 
 > *${team1Name}* 🏏: ${team1Score}
@@ -122,43 +120,18 @@ async function connectToWhatsApp() {
 
 © ⚽ *SPORTS WORLD*🏏
 
-https://chat.whatsapp.com/C2T0r1c2vLj8RdC3CII2Ky
-                                `;
+https://chat.whatsapp.com/C2T0r1c2vLj8RdC3CII2Ky`;
 
-                                sock.sendMessage(remoteJid, { text: `${liveUpdate}` });
-                            })
-                            .catch(error => {
-                                sock.sendMessage(remoteJid, { text: `🛑 *ERROR OCCURRED* 🛑\n\n> .livescore {match_id} {start_time}` });
-                                console.error('Error fetching match details:', error);
-                            });
+                            sock.sendMessage(remoteJid, { text: `${liveUpdate}` });
+                        } catch (error) {
+                            sock.sendMessage(remoteJid, { text: `🛑 *ERROR OCCURRED* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
+                            console.error('Error fetching match details:', error);
+                        }
                     });
 
                     activeTasks[remoteJid] = task;
                     task.start();
-                    sock.sendMessage(remoteJid, { text: `✅ *LIVE SCORE UPDATES* 🏏\n\n> Starting at: ${startTimeSL.format('HH:mm')} Sri Lanka Time` });
-                } else if (messageBody.startsWith(prefix + "matches")) {
-                    axios.get(API_URL)
-                        .then(response => {
-                            const matches = response.data.data;
-
-                            if (!matches || matches.length === 0) {
-                                console.error('No matches data found');
-                                sock.sendMessage(remoteJid, { text: `🛑 *NO MATCH DATA FOUND* 🛑\n\n> .livescore {match_id} {start_time}` });
-                                return;
-                            }
-
-                            const matchList = matches.map(match => {
-                                const team1Name = match.t1.match(/(.*?) \[/)[1];
-                                const team2Name = match.t2.match(/(.*?) \[/)[1];
-                                return `${team1Name} vs ${team2Name} *${match.id}*`;
-                            }).join('\n');
-
-                            sock.sendMessage(remoteJid, { text: matchList });
-                        })
-                        .catch(error => {
-                            sock.sendMessage(remoteJid, { text: `🛑 *ERROR OCCURRED* 🛑\n\n> .livescore {match_id} {start_time}` });
-                            console.error('Error fetching match list:', error);
-                        });
+                    sock.sendMessage(remoteJid, { text: `✅ *LIVE SCORE UPDATES* 🏏\n\n> Starting at: ${startTimeSL.format('HH:mm')} Sri Lanka Time\n> Update Duration: ${updateDuration} minutes` });
                 }
             }
         });
