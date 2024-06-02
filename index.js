3b19181a8dc3cbf9e2dd9a73549c3b7208df1b6c
Abdullasl321 <108709846+Abdullasl321@users.noreply.github.com>
2024-06-02 07:36:31 +0530

    PICXGRAPHY | UNIQUE WORLD

--- index.js
+++ index.js
@@ -67,96 +67,97 @@ async function connectToWhatsApp() {
         messages.forEach(async (msg) => {
 
 
-                if(grpmsg.startsWith(prefix + "alive")){
-                    sock.sendMessage(remoteJid, {text:"*CricBot is alive now!*"})
+            if (grpmsg.startsWith(prefix + "alive")){
+                sock.sendMessage(remoteJid, {text:"*CricBot is alive now!*"});
+            }
+            
+            if (grpmsg.startsWith(prefix + "livescore")) {
+                const args = grpmsg.split(" ");
+                const command = args[1];
+            
+                if (command === "stop") {
+                    if (activeTasks[remoteJid]) {
+                        activeTasks[remoteJid].stop();
+                        delete activeTasks[remoteJid];
+                        sock.sendMessage(remoteJid, { text: `🛑 *LIVE SCORE UPDATES STOPPED* 🛑` });
+                    } else {
+                        sock.sendMessage(remoteJid, { text: `🛑 *NO ACTIVE LIVE SCORE UPDATES* 🛑` });
+                    }
+                    return;
                 }
-
-                if (grpmsg.startsWith(prefix + "livescore")) {
-                    const args = grpmsg.split(" ");
-                    const command = args[1];
-                    
-                    if (command === "stop") {
-                        if (activeTasks[remoteJid]) {
-                            activeTasks[remoteJid].stop();
-                            delete activeTasks[remoteJid];
-                            sock.sendMessage(remoteJid, { text: `🛑 *LIVE SCORE UPDATES STOPPED* 🛑` });
-                        } else {
-                            sock.sendMessage(remoteJid, { text: `🛑 *NO ACTIVE LIVE SCORE UPDATES* 🛑` });
+            
+                const matchId = command;
+                const startTime = args[2];
+                const updateDuration = args[3] || '5'; // Default update duration is 5 minutes
+                if (!matchId || !startTime) {
+                    sock.sendMessage(remoteJid, { text: `🛑 *MATCH ID OR START TIME NOT PROVIDED* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
+                    return;
+                }
+            
+                const startTimeSL = moment.tz(startTime, 'HH:mm', 'Asia/Colombo');
+                console.log(`Starting live score updates for match ${matchId} at ${startTimeSL.format()}`);
+            
+                const task = cron.schedule(`*/${updateDuration} * * * *`, async () => {
+                    const nowSL = moment.tz('Asia/Colombo');
+                    if (nowSL.isBefore(startTimeSL)) return;
+            
+                    console.log(`Fetching live score for match ${matchId} at ${nowSL.format()}`);
+            
+                    try {
+                        const response = await axios.get(`${API_URL}&id=${matchId}`);
+                        const matches = response.data.data;
+            
+                        if (!matches || matches.length === 0) {
+                            console.error('No matches data found');
+                            sock.sendMessage(remoteJid, { text: `🛑 *NO MATCH DATA FOUND* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
+                            return;
                         }
-                        return;
-                    }
-
-                    const matchId = command;
-                    const startTime = args[2];
-                    const updateDuration = args[3] || '5'; // Default update duration is 5 minutes
-                    if (!matchId || !startTime) {
-                        sock.sendMessage(remoteJid, { text: `🛑 *MATCH ID OR START TIME NOT PROVIDED* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
-                        return;
-                    }
-
-                    const startTimeSL = moment.tz(startTime, 'HH:mm', 'Asia/Colombo');
-
-                    const task = cron.schedule(`*/${updateDuration} * * * *`, async () => {
-                        const nowSL = moment.tz('Asia/Colombo');
-                        if (nowSL.isBefore(startTimeSL)) return;
-
-                        try {
-                            const response = await axios.get(`${API_URL}&id=${matchId}`);
-                            const matches = response.data.data;
-
-                            if (!matches || matches.length === 0) {
-                                console.error('No matches data found');
-                                sock.sendMessage(remoteJid, { text: `🛑 *NO MATCH DATA FOUND* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
-                                return;
-                            }
-
-                            const match = matches.find(m => m.id === matchId);
-
-                            if (!match) {
-                                console.error('No match data found');
-                                sock.sendMessage(remoteJid, { text: `🛑 *NO MATCH DATA FOUND* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
-                                return;
-                            }
-
-                            const team1 = match.t1;
-                            const team2 = match.t2;
-                            const status = match.status;
-                            const series = match.series;
-                            const team1Score = match.t1s || 'N/A';
-                            const team2Score = match.t2s || 'N/A';
-
-                            const team1Name = team1.match(/(.*?) \[/)[1];
-                            const team2Name = team2.match(/(.*?) \[/)[1];
-
-                            
-
-                            const liveUpdate = `            🏏 *𝘓𝘐𝘝𝘌 𝘜𝘗𝘋𝘈𝘛𝘌𝘚* 🏏
-
+            
+                        const match = matches.find(m => m.id === matchId);
+            
+                        if (!match) {
+                            console.error('No match data found');
+                            sock.sendMessage(remoteJid, { text: `🛑 *NO MATCH DATA FOUND* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
+                            return;
+                        }
+            
+                        const team1 = match.t1;
+                        const team2 = match.t2;
+                        const status = match.status;
+                        const series = match.series;
+                        const team1Score = match.t1s || 'N/A';
+                        const team2Score = match.t2s || 'N/A';
+            
+                        const team1Name = team1.match(/(.*?) \[/)[1];
+                        const team2Name = team2.match(/(.*?) \[/)[1];
+            
+                        const liveUpdate = `     🏏 *𝘓𝘐𝘝𝘌 𝘜𝘗𝘋𝘈𝘛𝘌𝘚* 🏏
+            
 *${team1Name}* vs *${team2Name}*
-(${series})
-
+    (${series})
+            
 > *${team1Name}* 🏏: ${team1Score}
 > *${team2Name}* 🏏: ${team2Score}
-
+            
 ✉️ ${status}
-
+            
 © ⚽ *SPORTS WORLD*🏏
-
+            
 https://chat.whatsapp.com/C2T0r1c2vLj8RdC3CII2Ky`;
-
-                            sock.sendMessage(remoteJid, { text: `${liveUpdate}` });
-                        } catch (error) {
-                            sock.sendMessage(remoteJid, { text: `🛑 *ERROR OCCURRED* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
-                            console.error('Error fetching match details:', error);
-                        }
-                    });
-
-                    activeTasks[remoteJid] = task;
-                    task.start();
-                    sock.sendMessage(remoteJid, { text: `✅ *LIVE SCORE UPDATES* 🏏\n\n> Starting at: ${startTimeSL.format('HH:mm')} Sri Lanka Time\n> Update Duration: ${updateDuration} minutes` });
+            
+                        sock.sendMessage(remoteJid, { text: `${liveUpdate}` });
+                    } catch (error) {
+                        sock.sendMessage(remoteJid, { text: `🛑 *ERROR OCCURRED* 🛑\n\n> .livescore {match_id} {start_time} {update_duration_time}` });
+                        console.error('Error fetching match details:', error);
                     }
                 });
+            
+                activeTasks[remoteJid] = task;
+                task.start();
+                sock.sendMessage(remoteJid, { text: `✅ *LIVE SCORE UPDATES* 🏏\n\n> Starting at: ${startTimeSL.format('HH:mm')} Sri Lanka Time\n> Update Duration: ${updateDuration} minutes` });
             }
+        });
+    }            
         }
     });
 }
